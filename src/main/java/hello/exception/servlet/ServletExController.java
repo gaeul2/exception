2b99040184001d1e8e30b67b7ab943e79136278c
0540 <0540@coreit.co.kr>
2024-07-19 17:46:24 +0900

    서블릿 예외처리 - 오류페이지 1

--- src/main/java/hello/exception/servlet/ServletExController.java
+++ src/main/java/hello/exception/servlet/ServletExController.java
@@ -16,6 +16,10 @@ public class ServletExController {
     public void errorEx(){
         throw new RuntimeException("예외 발생!");
     }
+    @GetMapping("/error-400")
+    public void error400(HttpServletResponse response) throws IOException {
+        response.sendError(400,"400오류!");
+    }
 
     @GetMapping("/error-404")
     public void error404(HttpServletResponse response) throws IOException {
